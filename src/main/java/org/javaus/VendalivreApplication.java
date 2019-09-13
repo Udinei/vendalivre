364704c88bd0ca57a5f36c1ca11ea9c65df7d76a
Udinei Silva <ukaliko@gmail.com>
2019-09-13 18:26:57 -0400

    Projeto Criado

---
+++ src/main/java/org/javaus/VendalivreApplication.java
@@ -0,0 +1,13 @@
+package org.javaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendalivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendalivreApplication.class, args);
+	}
+
+}
